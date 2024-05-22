ee3fb6f7e6f1bd4b3fce63ed62d481463725bab6
cwait00 <cwaitanobongoza@gmail.com>
2024-05-22 11:18:27 +0000

    -a

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,49 +1,59 @@
 #!/usr/bin/env python3
-""" MRUCache module
+"""
+MRUCache module implementing a Most Recently Used (MRU) caching system.
 """
 
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache class
+    """
+    MRUCache class that inherits from BaseCaching
+    and implements an MRU caching system.
     """
 
     def __init__(self):
-        """ Initialize
+        """
+        Initialize the MRUCache class.
         """
         super().__init__()
-        self.usage_order = []
+        self.order = []
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item to the cache using the MRU algorithm.
+
+        Args:
+            key (str): The key of the item.
+            item (Any): The item to be stored in the cache.
         """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            self.usage_order.remove(key)
+            self.order.remove(key)
         elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            most_recently_used = self.usage_order.pop()
-            del self.cache_data[most_recently_used]
-            print(f"DISCARD: {most_recently_used}")
+            most_recently_used_key = self.order.pop()
+            del self.cache_data[most_recently_used_key]
+            print(f"DISCARD: {most_recently_used_key}")
 
         self.cache_data[key] = item
-        self.usage_order.append(key)
+        self.order.append(key)
 
     def get(self, key):
-        """ Get an item by key
         """
-        if key in self.cache_data:
-            self.usage_order.remove(key)
-            self.usage_order.append(key)
-            return self.cache_data[key]
-        else:
+        Retrieve an item from the cache.
+
+        Args:
+            key (str): The key of the item to retrieve.
+
+        Returns:
+            Any: The item associated with the key,
+            or None if the key is not found.
+        """
+        if key is None or key not in self.cache_data:
             return None
 
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in self.cache_data:
-            print(f"{key}: {self.cache_data[key]}")
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data[key]
